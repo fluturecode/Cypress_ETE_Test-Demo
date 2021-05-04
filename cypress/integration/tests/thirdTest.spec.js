6c959e6467b216cb700c00f11823fce6000be68a
Elizabeth Eidelson <fluturecode@gmail.com>
2021-05-03 23:36:05 -0400

    added thirTest.js

---
+++ cypress/integration/tests/thirdTest.spec.js
@@ -0,0 +1,18 @@
+describe(
+	"login",
+	{
+		retries: {
+			runMode: 3,
+			openMode: 2,
+		},
+	},
+	() => {
+		it("should redirect unauthenticated user to sign-in page", () => {
+			// ...
+		})
+
+		it("allows user to login", () => {
+			// ...
+		})
+	}
+)
